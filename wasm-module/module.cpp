8d38b667792bcb9f94932ba050ed7eab5b60641c
Joel A. Jaffe <joel@jaffesd.com>
2026-02-09 21:36:10 -0800

    NAM: Block-based processing (~2x speedup)

--- wasm-module/module.cpp
+++ wasm-module/module.cpp
@@ -8,17 +8,15 @@ extern "C" {
 void process(float* input, float* output, int num_samples) {
     static bool initialized = false;
     static MarshallModelWeights mWeights;
-    static wavenet::RTWavenet<1, 1, Layer1, Layer2> mModel;
+    static wavenet::RTWavenet<1, 128, Layer1, Layer2> mModel;
     // Lazy initialization on first call
     if (!initialized) {
         mModel.loadModel(mWeights.weights);
         initialized = true;
     }
     
-    // Process samples one at a time using the underlying model's forward method
-    for (int i = 0; i < num_samples; i++) {
-        output[i] = mModel.model.forward(input[i]);
-    }
+    // Process the entire block at once using the model's block-based forward method
+    mModel.model.forward(input, output, num_samples);
 }
 
 } // extern "C"
